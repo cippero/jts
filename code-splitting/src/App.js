33a84735b13ae81b890d7379dc7a19465edc1d41
cippero <gilwein@gmail.com>
2018-10-16 12:58:43 -0700

    setting up code splitting

--- code-splitting/src/App.js
+++ code-splitting/src/App.js
@@ -1,27 +1,35 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Page1 from './components/Page1';
+// import Page2 from './components/Page2';
+// import Page3 from './components/Page3';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = { 
+      route: 'page1',
+      component: null
+    }
+  }
+
+  onRouteChange = (route) => {
+    if (route === 'page1') this.setState({route});
+    else if (route === 'page2') {
+      import('./components/Page2')
+      .then(Page2 => this.setState({ component: Page2.default}));
+    }
+    else if (route === 'page3') {
+      import('./components/Page3')
+      .then(Page3 => this.setState({ component: Page3.default}));
+    }
+  }
+
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+    if (this.state.route === 'page1') return <Page1 onRouteChange={this.onRouteChange}/>
+    else {
+      return <this.state.component onRouteChange={this.onRouteChange} />
+    }
   }
 }
 
