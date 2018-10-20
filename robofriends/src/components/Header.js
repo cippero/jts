0127f62974bafdbab5d2f650cfacbaf8c0e69717
cippero <gilwein@gmail.com>
2018-10-19 17:33:16 -0700

    writing react tests with enzyme

--- robofriends/src/components/Header.js
+++ robofriends/src/components/Header.js
@@ -1,12 +1,22 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
+import CounterButton from './CounterButton';
+
+// In most cases, instead of writing shouldComponentUpdate()
+// by hand, you can inherit from React.PureComponent. It is
+// equivalent to implementing shouldComponentUpdate() with a
+// shallow comparison of current and previous props and state.
 
 export default class Header extends Component {
     shouldComponentUpdate(nextProps, nextState) {
         return false
     }
-
     render() {
-        // console.log('header');
-        return <h1 className='f1'>RoboFriends</h1>
+        console.log('header')
+        return (
+        <div>
+            <h1 className='f1'>RoboFriends</h1>
+            <CounterButton />
+        </div>
+        );
     }
-}
+};
---
+++ robofriends/src/containers/App.test.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { shallow } from 'enzyme'
+import App from './App';
+
+it('expect to render App component', () => {
+    // expect(shallow(<App />)).toMatchSnapshot();
+    expect(1).toEqual(1);
+});
