a4928bab67128ce7e9ef11b7a1a4f992aad72e1c
cippero <gilwein@gmail.com>
2018-10-24 14:33:52 -0700

    next.js practice

---
+++ .next/static/webpack/static/development/pages/index.js.ddee8bee2bc00262eb20.hot-update.js
@@ -0,0 +1,38 @@
+webpackHotUpdate("static/development/pages/index.js",{
+
+/***/ "./components/image.js":
+/*!*****************************!*\
+  !*** ./components/image.js ***!
+  \*****************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "./node_modules/react/index.js");
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
+var _jsxFileName = "/home/gilwein/code/projects/udemy_jts/next-ssr/components/image.js";
+
+
+var Image = function Image() {
+  return react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("img", {
+    src: "https://www.fws.gov/fisheries/freshwater-fish-of-america/images/originals/east_temperate/American_eel_DuaneRavenArt.png",
+    alt: "eel",
+    width: "500",
+    style: {
+      margin: '100px'
+    },
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 2
+    },
+    __self: this
+  });
+};
+
+/* harmony default export */ __webpack_exports__["default"] = (Image);
+
+/***/ })
+
+})
+//# sourceMappingURL=index.js.ddee8bee2bc00262eb20.hot-update.js.map
