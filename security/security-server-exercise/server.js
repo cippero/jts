d537cde2a659d271481e0518950909d09d09b452
cippero <gilwein@gmail.com>
2018-10-25 14:52:56 -0700

    learning security best practices

---
+++ security/security-server-exercise/server.js
@@ -0,0 +1,37 @@
+const express    = require('express');
+const cors       = require('cors');
+const helmet     = require('helmet');
+const morgan     = require('morgan');
+const winston    = require('winston');
+const bodyParser = require('body-parser');
+const uuidv4     = require('uuid/v4');
+const app = express();
+
+app.use(cors());
+app.use(morgan('tiny'));
+app.use(helmet());
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+  const id = uuidv4();
+  res.cookie('session', id, { httpOnly: true });
+  res.cookie('session', id, { secure: true });
+  res.set({
+    'Content-Security-Policy': 'script-src "self" "https://apis.google.com"'
+  });
+  res.send('Hello World!');
+})
+
+app.post('/secret', (req, res) => {
+  const { userInput } = req.body;
+  console.log({userInput});
+  if (userInput) {
+    winston.log('info', 'user input: ' + userInput);
+    res.status(200).json('success');
+  } else {
+    winston.error('This guy is messing with us:' + userInput);
+    res.status(400).json('incorrect submission')
+  }
+})
+
+app.listen(3000, () => console.log('Example app listening on port 3000!'))
