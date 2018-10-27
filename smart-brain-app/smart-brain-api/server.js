cb59f58eab727fbcf9491e38664057ab358c6420
cippero <gilwein@gmail.com>
2018-10-27 13:26:45 -0700

    docker practice

--- smart-brain-app/smart-brain-api/server.js
+++ smart-brain-app/smart-brain-api/server.js
@@ -4,6 +4,7 @@ const bodyParser = require('body-parser');
 const bcrypt = require('bcrypt-nodejs');
 const cors = require('cors');
 const knex = require('knex');
+const morgan = require('morgan');
 
 const register = require('./controllers/register');
 const signin = require('./controllers/signin');
@@ -22,10 +23,11 @@ const db = knex({
 
 const app = express();
 
+app.use(morgan('combined'));
 app.use(cors())
 app.use(bodyParser.json());
 
-app.get('/', (req, res)=> { res.send(database.users) })
+app.get('/', (req, res)=> { /* res.send(db.users) */ res.send('Hello World!!!!') })
 app.post('/signin', signin.handleSignin(db, bcrypt))
 app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) })
 app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db)})
